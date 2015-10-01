6e18d681080ec03197ee2fa835185c8e7ef9b0b8
unknown <cco207@AN112504-CT103.nvstu.edu>
2015-10-01 16:45:27 -0400

    Chris O'Brien 3.5

--- SwitchCase.java
+++ SwitchCase.java
@@ -4,32 +4,34 @@ import java.util.Scanner;
  * This program takes the input of the user's role at a school and welcomes them
  * It is purely an academic exercise to learn how to use switch case
  */
+import javax.swing.JOptionPane;
 public class SwitchCase {
 	public static void main(String [] args)
 	{
 		//The user enters their role
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter your role.");
-		String role = keyboard.nextLine();
-		keyboard.close();
+
+		String role = JOptionPane.showInputDialog(null, "Please specify your role from the following list: Administrator, Faculty, Staff, Student or Guest.");
+
+
+		
 		
 		//The program welcomes the user based off of the inputed role
 		switch(role)
 		{
 			case "Administrator":
-				System.out.println("Welcome " + role +"!");
+				JOptionPane.showMessageDialog(null,"Welcome " + role +"!");
 				break;
 			case "Faculty":
-				System.out.println("Welcome " + role +"!");
+				JOptionPane.showMessageDialog(null,"Welcome " + role +"!");
 				break;
 			case "Staff":
-				System.out.println("Welcome " + role +"!");
+				JOptionPane.showMessageDialog(null,"Welcome " + role +"!");
 				break;
 			case "Student":
-				System.out.println("Welcome " + role +"!");
+				JOptionPane.showMessageDialog(null, "Welcome " + role +"!");
 				break;
 			case "Guest":
-				System.out.println("Welcome " + role +"!");
+				JOptionPane.showMessageDialog(null, "Welcome " + role +"!");
 				break;
 		}
 	}
